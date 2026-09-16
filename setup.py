f107eadb1f4c322d74ab8a9962b032612f1357d5
wangtianyu61 <tw2837@columbia.edu>
2026-09-15 20:48:49 -0400

    Bump version to 0.4.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ md_path = os.path.join(here, 'src/README.md')
 
 setup(
     name='dro',
-    version='0.3.3',    
+    version='0.4.1',    
     license='MIT License',
     description='A package of distributionally robust optimization (DRO) methods. Implemented via cvxpy and PyTorch',
     long_description=open(md_path, encoding='utf-8').read(),
